abc6953c77fcb5a3bd29cef030463f0c3d554296
Aydyn Aliev <aliyev_aydin95@mail.ru>
2019-01-30 16:18:46 +0300

    init commit

--- src/main/java/com/epam/learnspring/Application.java
+++ src/main/java/com/epam/learnspring/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(value = "com.epam.learnspring")
+@ComponentScan(value = "com.epam.learnspring.controller")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/epam/learnspring/controller/AppController.java
+++ src/main/java/com/epam/learnspring/controller/AppController.java
@@ -1,33 +1,15 @@
 package com.epam.learnspring.controller;
 
-import com.epam.learnspring.model.Cat;
-import com.epam.learnspring.model.CatService;
-import com.epam.learnspring.model.Message;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AppController {
-    @Autowired
-    @Value("Murzik")
-    private Message message;
-
-    @Autowired
-    private CatService catService;
-
-    private ApplicationContext applicationContext = new ClassPathXmlApplicationContext("ioc.xml");
-    private Cat barsik = applicationContext.getBean("cat", Cat.class);
 
     @RequestMapping("/cat")
     public String getCatInfo(Model model) {
-        model.addAttribute("name", catService.getName());
-        model.addAttribute("nameFromXml", barsik.getName());
+        model.addAttribute("name", "Hello world!");
         return "cat";
     }
 }
